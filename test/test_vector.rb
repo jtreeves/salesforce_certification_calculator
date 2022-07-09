c7f57992529be6f2f71adb4b7bf5a12614849edb
jtreeves <jackson.t.reeves@gmail.com>
2022-07-09 18:23:45 -0400

    [test] Confirm get_vector works

--- test/test_vector.rb
+++ test/test_vector.rb
@@ -2,6 +2,14 @@ require "minitest/autorun"
 require "salesforce_certification_calculator/vector"
 
 class VectorTest < Minitest::Test
+    def test_get_vector
+        initial = [2, 3]
+        vector = SalesforceCertificationCalculator::Vector.new(initial)
+        extracted = vector.get_vector
+
+        assert_equal extracted, initial
+    end
+
     def test_dot_product_integers
         vector1 = SalesforceCertificationCalculator::Vector.new([2, 3])
         vector2 = SalesforceCertificationCalculator::Vector.new([4, 5])
@@ -10,7 +18,7 @@ class VectorTest < Minitest::Test
         assert_equal product, 23
     end
     
-    def test_dot_product_scores
+    def test_dot_product_decimals
         vector1 = SalesforceCertificationCalculator::Vector.new([0.4, 0.6])
         vector2 = SalesforceCertificationCalculator::Vector.new([0.8, 0.5])
         product = vector1.dot_product(vector2)
