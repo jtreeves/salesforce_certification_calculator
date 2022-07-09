cde886b9ef309fbb88f99f7a02e8551a3971bd54
jtreeves <jackson.t.reeves@gmail.com>
2022-07-09 18:44:33 -0400

    [vector] Round elements in scalar_multiplication array to 8 decimal places

--- lib/salesforce_certification_calculator/vector.rb
+++ lib/salesforce_certification_calculator/vector.rb
@@ -8,6 +8,18 @@ class SalesforceCertificationCalculator::Vector
         return @vector
     end
 
+    def scalar_multiplication(scalar)
+        product = []
+
+        (0..@length-1).each do |i|
+            initial_product = @vector[i] * scalar
+            rounded_product = initial_product.round(8)
+            product[i] = rounded_product
+        end
+
+        return product
+    end
+
     def dot_product(other_vector)
         sum = 0
         extracted_other_vector = other_vector.get_vector
@@ -19,15 +31,5 @@ class SalesforceCertificationCalculator::Vector
         rounded_sum = sum.round(4)
 
         return rounded_sum
-    end
-    
-    def scalar_multiplication(scalar)
-        product = []
-
-        (0..@length-1).each do |i|
-            product[i] = @vector[i] * scalar
-        end
-
-        return product
     end
 end
