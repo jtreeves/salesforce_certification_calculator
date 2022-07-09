a1f617821808fc8f94102dd4970e494ed3dcbea4
jtreeves <jackson.t.reeves@gmail.com>
2022-07-09 18:20:42 -0400

    [test] Add tests for dot_product with integers and decimals

--- test/test_salesforce_certification_calculator.rb
+++ test/test_salesforce_certification_calculator.rb
@@ -2,9 +2,10 @@ require "minitest/autorun"
 require "salesforce_certification_calculator"
 
 class SalesforceTest < Minitest::Test
-    def test_salesforce_score
+    def test_cumulative_score
         calculator = SalesforceCertificationCalculator.new([20, 80], [70, 60])
         score = calculator.cumulative_score
+        
         assert_equal score, 62
     end
 end
--- test/test_vector.rb
+++ test/test_vector.rb
@@ -1,11 +1,20 @@
-# require "minitest/autorun"
-# require "salesforce_certification_calculator/vector"
+require "minitest/autorun"
+require "salesforce_certification_calculator/vector"
 
-# class VectorTest < Minitest::Test
-#     def test_dot_product_integers
-#         vector1 = Vector.new([2, 3])
-#         vector2 = Vector.new([4, 5])
-#         product = vector1.dot_product(vector2)
-#         assert_equal product, 23
-#     end
-# end
+class VectorTest < Minitest::Test
+    def test_dot_product_integers
+        vector1 = SalesforceCertificationCalculator::Vector.new([2, 3])
+        vector2 = SalesforceCertificationCalculator::Vector.new([4, 5])
+        product = vector1.dot_product(vector2)
+        
+        assert_equal product, 23
+    end
+    
+    def test_dot_product_scores
+        vector1 = SalesforceCertificationCalculator::Vector.new([0.4, 0.6])
+        vector2 = SalesforceCertificationCalculator::Vector.new([0.8, 0.5])
+        product = vector1.dot_product(vector2)
+
+        assert_equal product, 0.62
+    end
+end
