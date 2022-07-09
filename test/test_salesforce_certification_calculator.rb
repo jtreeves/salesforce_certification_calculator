b565e1c0fce093b5d769ed318db18453a7ad0963
jtreeves <jackson.t.reeves@gmail.com>
2022-07-09 18:31:35 -0400

    [test] Create aliases for class imports

--- test/test_salesforce_certification_calculator.rb
+++ test/test_salesforce_certification_calculator.rb
@@ -2,10 +2,12 @@ require "minitest/autorun"
 require "salesforce_certification_calculator"
 
 class SalesforceTest < Minitest::Test
+    SFC = SalesforceCertificationCalculator
+
     def test_cumulative_score
-        calculator = SalesforceCertificationCalculator.new([20, 80], [70, 60])
+        calculator = SFC.new([20, 80], [70, 60])
         score = calculator.cumulative_score
-        
+
         assert_equal score, 62
     end
 end
--- test/test_vector.rb
+++ test/test_vector.rb
@@ -2,25 +2,27 @@ require "minitest/autorun"
 require "salesforce_certification_calculator/vector"
 
 class VectorTest < Minitest::Test
-    def test_get_vector
-        initial = [2, 3]
-        vector = SalesforceCertificationCalculator::Vector.new(initial)
-        extracted = vector.get_vector
+    Vector = SalesforceCertificationCalculator::Vector
 
-        assert_equal extracted, initial
+    def test_get_vector
+        initial_vector = [2, 3]
+        packaged_vector = Vector.new(initial_vector)
+        extracted_vector = packaged_vector.get_vector
+
+        assert_equal extracted_vector, initial_vector
     end
 
     def test_dot_product_integers
-        vector1 = SalesforceCertificationCalculator::Vector.new([2, 3])
-        vector2 = SalesforceCertificationCalculator::Vector.new([4, 5])
+        vector1 = Vector.new([2, 3])
+        vector2 = Vector.new([4, 5])
         product = vector1.dot_product(vector2)
         
         assert_equal product, 23
     end
     
     def test_dot_product_decimals
-        vector1 = SalesforceCertificationCalculator::Vector.new([0.4, 0.6])
-        vector2 = SalesforceCertificationCalculator::Vector.new([0.8, 0.5])
+        vector1 = Vector.new([0.4, 0.6])
+        vector2 = Vector.new([0.8, 0.5])
         product = vector1.dot_product(vector2)
 
         assert_equal product, 0.62
