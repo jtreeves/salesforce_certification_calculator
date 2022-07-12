ba3e88639b40c5d7788618bfaea6bb6b9fb4ff6e
jtreeves <jackson.t.reeves@gmail.com>
2022-07-12 19:40:40 -0400

    [u_i] Convert input to integer

--- lib/salesforce_certification_calculator/u_i.rb
+++ lib/salesforce_certification_calculator/u_i.rb
@@ -1,5 +1,5 @@
 class SalesforceCertificationCalculator::UI
-    def select_list_or_new(times = 0)
+    def self.select_list_or_new(times = 0)
         message = times == 0 ? "Do you want to select an exam from a list (enter LIST), or do you want to type in your own details (enter NEW)?" : "You must enter either LIST or NEW"
         puts message
         choice = gets.chomp
@@ -7,17 +7,17 @@ class SalesforceCertificationCalculator::UI
         if choice == "LIST" || choice == "NEW"
             return choice
         else
-            select_existing_exam(1)
+            select_list_or_new(1)
         end
     end
 
-    def select_specific_exam(exams)
+    def self.select_specific_exam(exams)
         puts "Your choices are:"
         exams.each do |exam|
             puts "#{exams.find_index(exam) + 1} - #{exam.get_title}"
         end
         puts "Which one would you like to select? Enter the number before the exam name:"
-        choice = gets.chomp
+        choice = gets.chomp.to_i
 
         if choice.between?(1, exams.length)
             return exams[choice - 1]
