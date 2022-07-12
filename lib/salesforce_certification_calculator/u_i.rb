8910f7f6c01e3f42b879b3b3fce27fccc9941a89
jtreeves <jackson.t.reeves@gmail.com>
2022-07-12 18:57:54 -0400

    [select_specific_exam] Given a list of exams, select one based on its index

--- lib/salesforce_certification_calculator/u_i.rb
+++ lib/salesforce_certification_calculator/u_i.rb
@@ -12,7 +12,25 @@ class SalesforceCertificationCalculator::UI
         else
             select_existing_exam(1)
         end
-        
+
         return selected_existing
+    end
+
+    def select_specific_exam(exams)
+        puts "Your choices are:"
+        exams.each do |exam|
+            puts "#{exams.find_index(exam) + 1} - #{exam.get_title}"
+        end
+        puts "Which one would you like to select? Enter the number before the exam name:"
+        input = gets.chomp
+        index
+
+        if input.between?(1, exams.length)
+            index = input - 1
+        else
+            select_specific_exam(exams)
+        end
+
+        return index
     end
 end
