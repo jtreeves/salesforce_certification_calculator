b0434f05da4fbb076bd48e563a7b5adf26e48552
jtreeves <jackson.t.reeves@gmail.com>
2022-07-12 21:49:43 -0400

    [determine_percentage] Create single method at root to implement all helper functions and return final answer

--- lib/salesforce_certification_calculator.rb
+++ lib/salesforce_certification_calculator.rb
@@ -1,7 +1,26 @@
 class SalesforceCertificationCalculator
-    def initialize(sections, scores)
-        @sections = Vector.new(sections)
-        @scores = Vector.new(scores)
+    def initialize
+        @exams = []
+        @exam = Exam.new
+        @accessor = Accessor.new
+        @ui = UI.new
+    end
+
+    def determine_percentage
+        choice = @ui.select_list_or_new
+
+        if choice == "LIST"
+            @exams = @accessor.get_exams_list
+            @exam = @ui.select_specific_exam(@exams)
+            @exam = @accessor.get_existing_exam_data(@exam)
+            @exam = @ui.retrieve_scores(@exam)
+        else
+            @exam = @ui.create_temporary_exam
+        end
+
+        @exam.calculate_total
+
+        return @exam.get_total
     end
 end
 
