7cccb1fd176d9ef08ecdfaf112281dbd33fb8b76
jtreeves <jackson.t.reeves@gmail.com>
2022-07-12 20:59:49 -0400

    [u_i] Add method for an exam without stored data

--- lib/salesforce_certification_calculator/u_i.rb
+++ lib/salesforce_certification_calculator/u_i.rb
@@ -1,4 +1,6 @@
 class SalesforceCertificationCalculator::UI
+    SFC = SalesforceCertificationCalculator
+
     def self.select_list_or_new(times = 0)
         message = times == 0 ? "Do you want to select an exam from a list (enter LIST), or do you want to type in your own details (enter NEW)?" : "You must enter either LIST or NEW"
         puts message
@@ -28,7 +30,7 @@ class SalesforceCertificationCalculator::UI
 
     def self.retrieve_scores(exam)
         sections = exam.get_sections
-        
+
         sections.each do |section|
             puts "#{section.get_name} is worth #{section.get_weight}%"
             puts "What score did you get on that section? (enter an integer)"
@@ -38,4 +40,23 @@ class SalesforceCertificationCalculator::UI
 
         return exam
     end
+
+    def self.create_temporary_exam
+        exam = SFC::Exam.new
+        puts "What is the title of this exam?"
+        exam.set_title(gets.chomp)
+        puts "How many sections does it have?"
+        length = gets.chomp.to_i
+        (1..length).each do |i|
+            puts "What is the name of Section #{i}"
+            name = gets.chomp
+            puts "How many percentage points is Section #{i} worth?"
+            weight = gets.chomp.to_i
+            puts "What score did you get on Section #{i}?"
+            score = gets.chomp.to_i
+            exam.add_section(name, weight, score)
+        end
+    end
 end
+
+require "salesforce_certification_calculator/exam"
