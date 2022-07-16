9aa160d4ea175434af9d69cdba44591be7d431e9
jtreeves <jackson.t.reeves@gmail.com>
2022-07-15 20:24:45 -0400

    [file_reader] Add documentation

--- lib/salesforce_certification_calculator/file_reader.rb
+++ lib/salesforce_certification_calculator/file_reader.rb
@@ -1,6 +1,14 @@
 class SalesforceCertificationCalculator::FileReader
     SFC = SalesforceCertificationCalculator
 
+    # Gets list of all exams in the database, including their names and file paths
+    # 
+    # @example Retrieve Exams
+    #   >> reader = FileReader.new
+    #   >> exams = reader.generate_exams_list
+    #   => [<SalesforceCertificationCalculator::Exam 0x000987>, <SalesforceCertificationCalculator::Exam 0x000988>]
+    # 
+    # @return [Array] collection of Exam objects
     def generate_exams_list
         exams = []
         files = Dir.glob("data/*xml")
@@ -17,6 +25,17 @@ class SalesforceCertificationCalculator::FileReader
         return exams
     end
 
+    # Gets all details about an exam's sections, including names and weights
+    # 
+    # @example Retrieve Exam Data
+    #   >> reader = FileReader.new
+    #   >> exams = FileReader.generate_exams_list
+    #   >> exam = reader.extract_exam_data(exams[0])
+    #   => <SalesforceCertificationCalculator::Exam 0x000987>
+    # 
+    # @param exam [Exam] object, containing name and file properties, to use to get full data
+    # 
+    # @return [Exam] object with not only name and file, but also all sections, with their names and weights
     def extract_initial_exam_data(exam)
         doc = File.open(exam.file) { |f| Nokogiri::XML(f) }
         names = doc.xpath("//name")
