636d7b96dbeda91331da900ab5041bdd3e01f650
jtreeves <jackson.t.reeves@gmail.com>
2022-07-15 21:31:47 -0400

    Add documentation at root

--- lib/salesforce_certification_calculator.rb
+++ lib/salesforce_certification_calculator.rb
@@ -1,6 +1,12 @@
 class SalesforceCertificationCalculator
+    # @!attribute [r] exam
+    #   @return [Exam] object being used for calculations
     attr_reader :exam
 
+    # Creates SalesforceCertificationCalculator object
+    # 
+    # @example Create Calculator Object
+    #   >> calculator = SalesforceCertificationCalculator.new
     def initialize
         @exams = []
         @exam = Exam.new
@@ -8,21 +14,52 @@ class SalesforceCertificationCalculator
         @ui = UI.new
     end
 
+    # Gets list of all exams in the database, including their names and file paths; shortcut for FileReader method to modify exams attribute on object
+    # 
+    # @example Retrieve Exams
+    #   >> calculator = SalesforceCertificationCalculator.new
+    #   >> exams = calculator.generate_exams_list
+    #   => [<SalesforceCertificationCalculator::Exam 0x000987>, <SalesforceCertificationCalculator::Exam 0x000988>]
     def generate_exams_list
         @exams = @reader.generate_exams_list
     end
 
+    # Gets all details about an exam's sections, including names and weights; shortcut for FileReader method to modify exam attribute on object
+    # 
+    # @example Retrieve Exam Data
+    #   >> calculator = SalesforceCertificationCalculator.new
+    #   >> exams = calculator.generate_exams_list
+    #   >> exam = calculator.extract_exam_data(exams[0])
+    #   => <SalesforceCertificationCalculator::Exam 0x000987>
+    # 
+    # @param exam [Exam] object, containing name and file properties, to use to get full data
     def extract_initial_exam_data
         @exam = @reader.extract_initial_exam_data(@exam)
     end
 
+    # Determines cumulative score for the exam based on its sections' weights and scores; shortcut for Exam method that returns total value of exam after calculating total
+    # 
+    # @example Calculate Total
+    #   >> calculator = SalesforceCertificationCalculator.new
+    #   >> total = calculator.calculate_total
+    #   => 87
+    # 
+    # @return [Number] cumulative score on exam
     def calculate_total
         @exam.calculate_total
 
         return @exam.total
     end
 
-    def determine_percentage
+    # Determines cumulative score manually; shortcut that combines all of UI's methods
+    # 
+    # @example Calculate Total
+    #   >> calculator = SalesforceCertificationCalculator.new
+    #   >> total = calculator.determine_percentage_manually
+    #   => 87
+    # 
+    # @return [Number] cumulative score on exam
+    def determine_percentage_manually
         choice = @ui.select_list_or_new
 
         if choice == "LIST"
